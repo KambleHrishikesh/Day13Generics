96c189735d4007266f45c43c53b6b2071c30cbbd
hrishikesh kamble <kamblehrishi13@gmail.com>
2022-06-05 21:10:42 +0530

    UC2 push

--- Program.cs
+++ Program.cs
@@ -4,7 +4,35 @@
     {
         public static void Main(string[]args)
         {
-            Console.WriteLine("Find Maximum of given 3");
+            Console.WriteLine("Find Maximum of given 3 Floats");
+            
+            Console.WriteLine("enter first value");
+            double firstvalue = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter second value");
+            double secondvalue = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter third value");
+            double thirdvalue = Convert.ToDouble(Console.ReadLine());
+
+            if (firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) > 0 ||
+                firstvalue.CompareTo(secondvalue) >= 0 && firstvalue.CompareTo(thirdvalue) > 0 ||
+                firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) >= 0)
+            {
+                Console.WriteLine("First value is greater");
+            }
+
+            if (secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0 ||
+                secondvalue.CompareTo(firstvalue) >= 0 && secondvalue.CompareTo(thirdvalue) > 0 ||
+                secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) >= 0)
+            {
+                Console.WriteLine("second value is greater");
+            }
+
+            if (thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) > 0 ||
+                thirdvalue.CompareTo(firstvalue) >= 0 && thirdvalue.CompareTo(secondvalue) > 0 ||
+                thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) >= 0)
+            {
+                Console.WriteLine("third value is greater");
+            }
 
         }
     }
