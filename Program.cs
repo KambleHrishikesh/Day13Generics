4decfbcc71f17aeff84b10125f5bb25a0a0edfea
hrishikesh kamble <kamblehrishi13@gmail.com>
2022-06-07 11:54:46 +0530

    generic class extented till 5 element push

--- Program.cs
+++ Program.cs
@@ -5,7 +5,16 @@
         public static void Main(string[]args)
         {
             Console.WriteLine("Find Maximum of given 3");
-
+            int[] intArray = { 100, 321, 900, 200, 250 };
+            GenericMaximum<int> generic = new GenericMaximum<int>(intArray);
+            generic.PrintMaxValue();
+            double[] doubleArray = { 10.2, 30.4, 4.42, 55.5, 6.87 };
+            GenericMaximum<double> genericDouble = new GenericMaximum<double>(doubleArray);
+            genericDouble.PrintMaxValue();
+            string[] stringArray = { "Apple", "Peach", "Banana", "Watermelon", "Guava" };
+            GenericMaximum<string> genericString = new GenericMaximum<string>(stringArray);
+            genericString.PrintMaxValue();
+            Console.ReadLine();
         }
     }
 
